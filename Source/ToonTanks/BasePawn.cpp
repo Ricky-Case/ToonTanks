6bd31be94ab4957b2d533193cb0b07bc0905cefa
Ricky-Case <raca226@uky.edu>
2024-06-18 11:11:39 -0400

    Lesson 115 complete.

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -1,7 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
 
 ABasePawn::ABasePawn()
 {
--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -16,6 +14,10 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY()
+	class UCapsuleComponent* CapsuleComp;
 
 public:
 	virtual void Tick(float DeltaTime) override;
